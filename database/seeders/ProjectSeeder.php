5ff8bd04bc5e9606b55b59ee16b67a643c62f3cd
Claudio-Pri <priv.claudio@gmail.com>
2024-10-29 16:44:36 +0100

    UserSeeder-ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+//per slug
+use Illuminate\Support\Str;
+
+//Models
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,6 +17,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Project::truncate();
+        for ($i = 0; $i < 5; $i++) {
+            $title = fake()->catchPhrase();
+            $slug = Str::of($title)->slug('-');
+            Project::create([
+                'title' => $title,
+                'slug' => $slug,
+                'url' => fake()->url(),
+                'description' => fake()->paragraph(),
+            ]);
+        }
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.guest')
 
+@section('page-title', 'Home')
+
 @section('main-content')
     <div class="row">
         <div class="col">
